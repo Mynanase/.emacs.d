1ad4c3e8c164116fa6bafb1cbadd337ed3d667a7
Nana <3547577912@qq.com>
2024-12-21 23:56:24 +0800

    d

--- custom.el
+++ custom.el
@@ -3,12 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ace-window benchmark-init cnfonts company crux doom-themes
-		drag-stuff embark-consult flycheck hungry-delete
-		marginalia orderless org-appear org-roam org-roam-ui
-		org-superstar org-typst-preview restart-emacs
-		smart-mode-line typst-ts-mode vertico)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
